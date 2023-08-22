f53e97b45d848f72df0a3b10219be581ce3d1c52
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-21 17:16:45 -0700

    Message

--- 03_colors.sh
+++ 03_colors.sh
@@ -3,3 +3,7 @@
 # The syntax to print is 
 # Ex:
  echo -e "\e[32m- your message to be printed \e[0m"
+ echo -e "\e[33m- your message to be printed \e[0m"
+ echo -e "\e[31m- your message to be printed \e[0m"
+ echo -e "\e[34m- your message to be printed \e[0m"
+ echo -e "\e[35m- your message to be printed \e[0m"
