c4116c209c3be7828265da4f4776fbbed39f3434
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-21 17:07:15 -0700

    Message

--- 01_Shebang.sh
+++ 01_Shebang.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 echo This is My First Scripts
 echo  Hello WORLD 123
+echo  line1
+echo  line2
---
+++ 02_echo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# \n : It tells the cusror to copy the cursor to the new line
+echo line1\nline2\tline3
