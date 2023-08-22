d671fa66c9f69aaf8d312aa97c9fdd0d5f5ecde4
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-21 17:08:56 -0700

    Message

--- 02_echo.sh
+++ 02_echo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # \n : It tells the cusror to copy the cursor to the new line
-echo line1\nline2\tline3
+echo \n "line1\nline2\tline3"
+
+
+#to use escape sequene character can be enable '-e'
