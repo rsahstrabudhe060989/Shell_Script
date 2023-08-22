07512126147d6b5a9786b47f2143c63ee0221ef4
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-21 17:44:07 -0700

    Message

--- 05_dynamicsvar.sh
+++ 05_dynamicsvar.sh
@@ -4,3 +4,4 @@ Today= ${date +%F}
 
 echo -e "Todays date is: \e[32m ${date} \e[0m"
 echo -e "Todays date is: \e[32m ${today} \e[0m"
+echo -e "The No of login users are: \e[32m ${who | wc -l} \e[0m"
