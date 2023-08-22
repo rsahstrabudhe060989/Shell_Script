af5ae13dc508e1a34bdc584b346b044b6aed8346
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-22 16:18:53 -0700

    Message

---
+++ 14_ifelse.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+<<comment
+if [ expresion1] ; then
+commands
+elif [experssion2] ; then
+coomands
+else 
+ command2
+fi
+
+Operators on number :
+-eq , -ne , -gt , -ge, -lt, -le
+Operatiors on strings = , ==, !=
+Operators  on files
+
+[ -f file] true of file exises and regular file
+[ -d xyz]  true if the file exist and it is directory
+
+[ -z "$var" ]   This is true if var is not having any date
+[ -n "$var" ]   This is true if var not having any date
+comment
+
+echo "Demonstrating simple if condtions"
+action $1
+
+if [ "$action" == "start"] ; then
+echo -e "Correct Value /start"
+else if [ "$action" == "stop"] ; then
+echo -e "Correct Value /stop"
+else 
+echo -e "wrong Value "
+if 
+
