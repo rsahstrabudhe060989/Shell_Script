48cffd3cd5f97cb69e290c7904a64c4bfbaec7e7
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-21 17:54:08 -0700

    Message

--- 05_dynamicsvar.sh
+++ 05_dynamicsvar.sh
@@ -6,3 +6,4 @@ echo -e "Todays date is: \e[32m ${date} \e[0m"
 echo -e "Todays date is: \e[32m ${today} \e[0m"
 echo  echo first varible $0
 echo  echo second varible $1
+echo  echo suplied  varible $#
