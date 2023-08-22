fbe9e484cb3adb010caddb1f879f3716e108c561
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-22 13:27:51 -0700

    Message

--- 11_case.sh
+++ 11_case.sh
@@ -6,12 +6,12 @@ action=$1
 case $action in
 
      start)
-      echo -e " \e[32m Starting Payment Service \e [0m]"
+      echo -e " \e[32m Starting Payment Service \e [0m"
       ;;
       stop) 
-      echo "Stopping Payment Service"
+      echo -e "\e [31m Stopping Payment Service \e [0m"
       ;;
       restart)
-      echo "Restarting payment service"
+      echo -e"\e[30m Restarting\e[om \e[33m payment service[0m"
 
 esac
