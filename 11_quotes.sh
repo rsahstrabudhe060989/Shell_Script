a6304af200239f739e84a481678075a4c9df26ba
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-22 13:06:16 -0700

    Message

---
+++ 11_quotes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# " " : They dont effect the power of special variable
+# ' ' : They will effect the power of special variable
+a=10
+
+echo "Printing the value of a:$a"
+
+echo 'Printing the value of a:$a'
