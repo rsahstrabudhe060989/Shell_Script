791b4b50445ae785d1ddab47f2f888406ecbdf1e
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-23 16:42:38 -0700

    Message

--- robot/Componentz/mongodb.sh
+++ robot/Componentz/mongodb.sh
@@ -37,7 +37,7 @@ systemctl start mongod
 stat $?
 
 echo -n "Updating the $component visibility"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
 echo -n "performing Daemon-Reload :"
