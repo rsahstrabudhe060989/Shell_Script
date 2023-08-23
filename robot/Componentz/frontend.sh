8855b43479e374a3ef4c1bf73df627b3bdd83f49
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-22 23:02:39 -0700

    Message

--- robot/Componentz/frontend.sh
+++ robot/Componentz/frontend.sh
@@ -8,16 +8,17 @@ if [ "$user_id" -ne 0] ; then
    echo "you should execute this script as a root user"
    exit 1
 fi
-yum install nginx -y
+yum install nginx -y &>> /tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
  cd /usr/share/nginx/html
- rm -rf *
+ rm -rf * &>> /tmp/frontend.log
  unzip /tmp/frontend.zip
  mv frontend-main/* .
  mv static/* .
  rm -rf frontend-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
- systemctl enable nginx
- systemctl start nginx
+ systemctl enable nginx &>> /tmp/frontend.log
+ systemctl start nginx &>> /tmp/frontend.log
+
