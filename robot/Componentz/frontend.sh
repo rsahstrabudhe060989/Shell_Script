ea7d5a7d604766c890382d34a63caf03e68577dc
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-22 23:00:01 -0700

    Message

--- robot/Componentz/frontend.sh
+++ robot/Componentz/frontend.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -r # if any thing fail script will be exited
+
+#validating  wheather the executed user is root or not
+user_id=${id -u} 
+if [ "$user_id" -ne 0] ; then
+   echo "you should execute this script as a root user"
+   exit 1
+fi
 yum install nginx -y
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
