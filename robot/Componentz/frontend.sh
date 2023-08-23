0255e6c62ed60ed9a289e56562a91b76085336ad
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-23 12:35:14 -0700

    Message

--- robot/Componentz/frontend.sh
+++ robot/Componentz/frontend.sh
@@ -18,17 +18,44 @@ if [ $? -eq 0 ] ; then
 echo -e "\e[32m success \e[0m"
 else
 echo "failure"
+exit 2
 fi
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -n "Downloading the frontned Component:"
 
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+echo -e "\e[32m success \e[0m"
+else
+echo "failure"
+exit 2
+fi
+
+echo -n "Performing Cleanup of Old frontend:"
  cd /usr/share/nginx/html
  rm -rf * &>> /tmp/frontend.log
+ 
+if [ $? -eq 0 ] ; then
+echo -e "\e[32m success \e[0m"
+else
+echo "failure"
+exit 2
+fi
+
+echo -n "Copying the downloaded frontend component:"
  unzip /tmp/frontend.zip &>>/tmp/frontend.log
  mv frontend-main/* .
  mv static/* .
  rm -rf frontend-main README.md
  mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ] ; then
+echo -e "\e[32m success \e[0m"
+else
+echo "failure"
+exit 2
+fi
+
+echo -n "Starting the Server"
  systemctl enable nginx &>> /tmp/frontend.log
  systemctl start nginx &>> /tmp/frontend.log
 
