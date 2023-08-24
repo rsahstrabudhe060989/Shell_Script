aa3c0337e99d3e3231769aa65fbd7241b4ee038c
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-24 15:21:05 -0700

    Message

--- robot/Componentz/catalogue.sh
+++ robot/Componentz/catalogue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 component=catalogue
-source componetz/common.sh # Source is load the file, so that you can call all them
+source componetz/Common.sh # Source is load the file, so that you can call all them
 NODEJS      #Calling Nodejs Function
