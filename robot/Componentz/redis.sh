f0191629c65c942c81a51971e284e0786d2779b1
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-24 12:55:06 -0700

    Message

--- robot/Componentz/redis.sh
+++ robot/Componentz/redis.sh
@@ -27,7 +27,7 @@ curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.r
 stat $?
 
 echo -n "Installing $component server"
-yum install redis-6.2.11 -y  
+yum install redis-6.2.13 -y  
 stat $?
 
 echo -n "Updating the $component visibility"
