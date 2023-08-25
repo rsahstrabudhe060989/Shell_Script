adfab31750daccd4a6f5c4f2318bca09d9970885
rsahstrabudhe060989 <ravindrasahstrabudhe@gmail.com>
2023-08-24 18:32:27 -0700

    Message

--- robot/Componentz/rabbitmq.sh
+++ robot/Componentz/rabbitmq.sh
@@ -1,3 +1,27 @@
-#/bin/bash
+#!/bin/bash
 
-echo " I am radis.sh"
+set -e # if any thing fail script will be exited
+
+#validating  wheather the executed user is root or not
+component=rabbitmq
+
+
+source Componentz/common.sh 
+
+ echo -n "Installation ERLang Dependency:"
+ curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>> $logfile
+ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $logfile
+ $?
+echo -n "Installation $component:"
+yum install rabbitmq-server -y
+ $?
+
+ systemctl enable rabbitmq-server 
+ systemctl start rabbitmq-server
+
+echo -n "Creating $component aaplication"
+
+ rabbitmqctl add_user roboshop roboshop123
+ rabbitmqctl set_user_tags roboshop administrator
+ rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+```
